0fe624e261aee913d19dd79358fe4be07feb6729
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2022-06-07 14:52:07 +0200

    update Turkey link

--- src/components/ordernow.js
+++ src/components/ordernow.js
@@ -13,7 +13,7 @@ function OrderNow(props) {
               "Germany":'https://www.foodpanda.de',
               "UAE":'https://www.talabat.com/uae/everyday-roastery-business-bay',
               "Qatar":'https://www.talabat.com/qatar/everyday-coffee-roastery',
-              "Turkey":'https://www.yemeksepeti.com/banabi/kategori/kahve/sicak-kahveler-2',
+              "Turkey":'https://www.yemeksepeti.com/darkstore/mcgw/yemeksepeti-market-19-mayis-istanbul/category/a4992ed8-3dc6-4be5-9e92-06b642c45787',
               'Egypt':'https://www.talabat.com/egypt/everyday-roastery-tagammoa-5--banks-center'
   }
 
